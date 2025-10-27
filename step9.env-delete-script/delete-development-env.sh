db889f382fc2bf4885b0bb438cc9cc50ab4507b3
k-tajika <k-tajika@jaist.ac.jp>
2025-10-27 13:32:43 +0900

    削除処理

--- step9.env-delete-script/delete-development-env.sh
+++ step9.env-delete-script/delete-development-env.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# === r760xs3（172.16.100.13）のVM停止・削除 ===
-ssh root@172.16.100.13 "qm stop 121 --timeout 30 || true"
-ssh root@172.16.100.13 "qm destroy 121 --purge"
+# === r760xs1（172.16.100.11）のVM停止・削除 ===
+ssh root@172.16.100.1 "qm    stop 121 --timeout 30 || true"
+ssh root@172.16.100.1 "qm destroy 121 --purge"
 
-ssh root@172.16.100.13 "qm stop 131 --timeout 30 || true"
-ssh root@172.16.100.13 "qm destroy 131 --purge"
+# === r760xs2（172.16.100.12）のVM停止・削除 ===
+ssh root@172.16.100.2 "qm    stop 122 --timeout 30 || true"
+ssh root@172.16.100.2 "qm destroy 122 --purge"
+
+# === r760xs3（172.16.100.13）のVM停止・削除 ===
+ssh root@172.16.100.3 "qm    stop 123 --timeout 30 || true"
+ssh root@172.16.100.3 "qm destroy 123 --purge"
 
 # === r760xs4（172.16.100.14）のVM停止・削除 ===
-ssh root@172.16.100.14 "qm stop 122 --timeout 30 || true"
-ssh root@172.16.100.14 "qm destroy 122 --purge"
-
-ssh root@172.16.100.14 "qm stop 132 --timeout 30 || true"
-ssh root@172.16.100.14 "qm destroy 132 --purge"
+ssh root@172.16.100.4 "qm    stop 124 --timeout 30 || true"
+ssh root@172.16.100.4 "qm destroy 124 --purge"
 
 # === r760xs5（172.16.100.15）のVM停止・削除 ===
-ssh root@172.16.100.15 "qm stop 123 --timeout 30 || true"
-ssh root@172.16.100.15 "qm destroy 123 --purge"
-
-ssh root@172.16.100.15 "qm stop 133 --timeout 30 || true"
-ssh root@172.16.100.15 "qm destroy 133 --purge"
+ssh root@172.16.100.5 "qm    stop 125 --timeout 30 || true"
+ssh root@172.16.100.5 "qm destroy 125 --purge"
