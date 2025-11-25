b9a77832c0cc9b1a82c3f674710beadb09cc0bef
k-tajika <k-tajika@jaist.ac.jp>
2025-11-25 16:07:57 +0900

    VM132 typo update

--- 2.create-environment/delete/delete-sandbox-env.sh
+++ 2.create-environment/delete/delete-sandbox-env.sh
@@ -13,9 +13,9 @@ fi
 ssh root@172.16.100.11 "qm    stop 131 --timeout 30 || true"
 ssh root@172.16.100.11 "qm destroy 131 --purge"
 
-# === r760xs2（172.16.100.13）のVM停止・削除 ===
-ssh root@172.16.100.13 "qm    stop 132 --timeout 30 || true"
-ssh root@172.16.100.13 "qm destroy 132 --purge"
+# === r760xs2（172.16.100.12）のVM停止・削除 ===
+ssh root@172.16.100.12 "qm    stop 132 --timeout 30 || true"
+ssh root@172.16.100.12 "qm destroy 132 --purge"
 
 # === r760xs3（172.16.100.13）のVM停止・削除 ===
 ssh root@172.16.100.13 "qm    stop 133 --timeout 30 || true"
