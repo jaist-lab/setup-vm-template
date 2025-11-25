fc09cc05cc728ce4ade9c91cecca66d71707bbae
k-tajika <k-tajika@jaist.ac.jp>
2025-11-25 17:02:28 +0900

    ssh化

--- 2.create-environment/startup-sandbox-cluster.sh
+++ 2.create-environment/startup-sandbox-cluster.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-qm start 131
-qm start 132
-qm start 133
-qm start 134
-qm start 135
+ssh jaist-lab@172.16.100.131 "qm start 131"
+ssh jaist-lab@172.16.100.132 "qm start 132"
+ssh jaist-lab@172.16.100.133 "qm start 133"
+ssh jaist-lab@172.16.100.134 "qm start 134"
+ssh jaist-lab@172.16.100.135 "qm start 135"
 
-qm status 131
-qm status 132
-qm status 133
-qm status 134
-qm status 135
+ssh jaist-lab@172.16.100.131 "qm status 131"
+ssh jaist-lab@172.16.100.132 "qm status 132"
+ssh jaist-lab@172.16.100.133 "qm status 133"
+ssh jaist-lab@172.16.100.134 "qm status 134"
+ssh jaist-lab@172.16.100.135 "qm status 135"
+
